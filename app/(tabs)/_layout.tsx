2709efb4c74875efec163f6a815613923be4c978
engrusmanbelloa <usmanbelloa@gmail.com>
2023-05-15 23:04:47 +0100

    main screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,56 +1,92 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable, useColorScheme } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons"
+import { Link, Tabs } from "expo-router"
+import { Pressable, useColorScheme } from "react-native"
 
-import Colors from "../../constants/Colors";
+import Colors from "../../constants/Colors"
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
+    name: React.ComponentProps<typeof FontAwesome5>["name"]
+    color: string
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome5 size={28} style={{ marginBottom: -3 }} {...props} />
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme()
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Tab One",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: "Tab Two",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="home" color={color} />
+                    ),
+                    headerRight: () => (
+                        <Link href="/modal" asChild>
+                            <Pressable>
+                                {({ pressed }) => (
+                                    <FontAwesome5
+                                        name="info-circle"
+                                        size={25}
+                                        color={
+                                            Colors[colorScheme ?? "light"].text
+                                        }
+                                        style={{
+                                            marginRight: 15,
+                                            opacity: pressed ? 0.5 : 1,
+                                        }}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="favorites"
+                options={{
+                    title: "Favorites",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="star" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="pay"
+                options={{
+                    title: "Pay",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="circle" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="wallets"
+                options={{
+                    title: "Wallets",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="wallet" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="menu"
+                options={{
+                    title: "Menu",
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="bars" color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    )
 }
