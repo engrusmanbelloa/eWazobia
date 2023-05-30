0197438ea571d0f45f8a5074edbe729111b303d4
engrusmanbelloa <usmanbelloa@gmail.com>
2023-05-30 12:08:45 +0100

    done onboarding

--- app/(onboarding)/welcome.tsx
+++ app/(onboarding)/welcome.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View, ImageBackground } from "react-native"
-
 import { Link, Stack, useRouter } from "expo-router"
 import styled from "styled-components/native"
 import { SafeAreaView } from "react-native-safe-area-context"
@@ -19,7 +18,7 @@ const LogoWhite = styled(Image)`
   width: 50%;
   height: 30px;
   position: absolute;
-  top: 5px;
+  top: 15px;
   left: 5px;
 `
 const Next = styled(Pressable)`
@@ -31,34 +30,63 @@ const Next = styled(Pressable)`
   left: 40px;
   opacity: 0;
 `
-
+const Title = styled(Text)`
+  color: #fff;
+  position: absolute;
+  top: 150px;
+  left: 40px;
+  font-size: 80px;
+  font-weight: 700;
+  line-height: 80px;
+`
 const Slogan = styled(Text)`
   color: #fff;
+  position: absolute;
+  top: 230px;
+  right: 10px;
   width: 200px;
   font-size: 15px;
   font-weight: 500;
   line-height: 20px;
 `
 
-const Title = styled(Text)`
+const Naira = styled(Text)`
   color: #fff;
-  font-size: 50px;
-  font-weight: 500;
-  line-height: 50px;
+  position: absolute;
+  bottom: 230px;
+  left: 100px;
+  font-size: 60px;
+  font-weight: 700;
+  line-height: 80px;
   margin-bottom: 10px;
 `
 
-const Side = styled(Box)`
+const ActivePlaces = styled(Text)`
+  color: #fff;
   position: absolute;
-  top: 180px;
-  right: 0px;
+  bottom: 140px;
+  left: 30px;
+  width: 130px;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 20px;
 `
+
+const NairaImg = styled(Image)`
+  width: 50%;
+  height: 25%;
+  position: absolute;
+  bottom: 15px;
+  right: 5px;
+`
+
 // background: linear-gradient(90deg, #228b22 46%, #732995 100%);
 
 export default function Welcome() {
   const router = useRouter()
   const bg = require("../../assets/images/welcomebg.png")
   const logo = require("../../assets/images/LogoWhite.png")
+  const naira = require("../../assets/images/naira.png")
 
   const handleNext = () => {
     router.push("/onboarding")
@@ -70,22 +98,13 @@ export default function Welcome() {
         <Stack.Screen options={{ title: "Welcome" }} />
         <BackgroundImg source={bg} resizeMode="cover">
           <LogoWhite source={logo} alt="Alternate Text" />
-          <Side>
-            <Box
-              bg={{
-                linearGradient: {
-                  colors: ["#0e32b4", "#228b22"],
-                  start: [0, 0],
-                  end: [1, 0],
-                },
-              }}
-            >
-              <Title>eWazobia</Title>
-              <Slogan>
-                Your one stop faster, secured payments starts with eWazobia.
-              </Slogan>
-            </Box>
-          </Side>
+          <Title>eWazobia</Title>
+          <Slogan>
+            Your one stop faster, secured payments starts with eWazobia.
+          </Slogan>
+          <Naira>eNaira</Naira>
+          <ActivePlaces>Currently active in 50+ universities</ActivePlaces>
+          <NairaImg source={naira} alt="Alternate Text" />
           <Next onPress={handleNext}></Next>
         </BackgroundImg>
       </Container>
